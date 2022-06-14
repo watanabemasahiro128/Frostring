94b1ef87ea97bc1380d92fb5cbe87a6a4efb84e5
D15143-Masahiro-NIT <d15143.masahiro.nit@gmail.com>
2022-06-15 02:15:43 +0900

    プレフィックスを表示するよう変更

--- src/diagnostics.ts
+++ src/diagnostics.ts
@@ -2,22 +2,25 @@
 
 import * as vscode from 'vscode';
 
+const decorationType = vscode.window.createTextEditorDecorationType({
+  after: {
+    color: '#808080',
+    backgroundColor: '#2F2F2F',
+    textDecoration: `;
+      margin: 0.1em 0.25em 0.1em 0.1em;
+      border: thin solid #606060;
+      border-radius: 0.2em;
+    `,
+  },
+});
 export const FROZEN_STRING_LITERAL = '# frozen_string_literal:';
 
-export function refreshDiagnostics(document: vscode.TextDocument, frostringDiagnostics: vscode.DiagnosticCollection): void {
-  if (
-    (document.languageId !== 'ruby' && document.languageId !== 'gemfile') ||
-    document.isUntitled ||
-    document.uri.scheme !== 'file'
-  ) {
-    return;
-  }
+function refreshDiagnostics(document: vscode.TextDocument, frostringDiagnostics: vscode.DiagnosticCollection): void {
+  if ((document.languageId !== 'ruby' && document.languageId !== 'gemfile') || document.uri.scheme !== 'file') return;
 
   const diagnostics: vscode.Diagnostic[] = [];
   const lineOfText = document.lineAt(0);
-  if (!lineOfText.text.includes(FROZEN_STRING_LITERAL)) {
-    diagnostics.push(createDiagnostic(document, lineOfText));
-  }
+  if (!lineOfText.text.includes(FROZEN_STRING_LITERAL)) diagnostics.push(createDiagnostic(document, lineOfText));
   frostringDiagnostics.set(document.uri, diagnostics);
 }
 
@@ -30,19 +33,50 @@ function createDiagnostic(document: vscode.TextDocument, lineOfText: vscode.Text
   return diagnostic;
 }
 
+function refreshDecorations(editor: vscode.TextEditor): void {
+  if ((editor.document.languageId !== 'ruby' && editor.document.languageId !== 'gemfile') || editor.document.uri.scheme !== 'file') return;
+
+  const options: vscode.DecorationOptions[] = [];
+  if (!editor.document.lineAt(0).text.includes(FROZEN_STRING_LITERAL)) options.push(createDecorationOptions());
+  editor.setDecorations(decorationType, options);
+}
+
+function createDecorationOptions(): vscode.DecorationOptions {
+  return {
+    range: new vscode.Range(0, 0, 0, 0),
+    renderOptions: {
+      after: {
+        contentText: 'Missing frozen string literal',
+      },
+    },
+  };
+}
+
 export function subscribeToDocumentChanges(context: vscode.ExtensionContext, frostringDiagnostics: vscode.DiagnosticCollection): void {
   if (vscode.window.activeTextEditor) {
+    refreshDecorations(vscode.window.activeTextEditor);
     refreshDiagnostics(vscode.window.activeTextEditor.document, frostringDiagnostics);
   }
   context.subscriptions.push(
     vscode.window.onDidChangeActiveTextEditor(
       editor => {
-        if (editor) {
-          refreshDiagnostics(editor.document, frostringDiagnostics);
-        }
+        if (!editor) return;
+
+        refreshDecorations(editor);
+        refreshDiagnostics(editor.document, frostringDiagnostics);
       }
     )
   );
-  context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(e => refreshDiagnostics(e.document, frostringDiagnostics)));
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeTextDocument(
+      event => {
+        const editor = vscode.window.visibleTextEditors.find(
+          (e) => e.document === event.document
+        );
+        if (editor) refreshDecorations(editor);
+        refreshDiagnostics(event.document, frostringDiagnostics);
+      }
+    )
+  );
   context.subscriptions.push(vscode.workspace.onDidCloseTextDocument(doc => frostringDiagnostics.delete(doc.uri)));
 }
